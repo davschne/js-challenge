48f9e8e47468ac7f4c9617df7198d7ac587b04f5
davschne <davschne@gmail.com>
2016-02-29 00:12:08 -0800

    persisting data in local storage

--- src/components/contacts-model.js
+++ src/components/contacts-model.js
@@ -1,16 +1,38 @@
 var contactsModel = function() {
 
-  var contacts = [];
-  var counter = 0; // used for assigning a unique ID to each contact
+  var contacts; // array for storing contact records
+  var counter; // used for assigning a unique ID to each contact
+
+  var init = function() {
+    // try retrieving data from local storage
+    var stored = JSON.parse(localStorage.getItem("contacts"));
+    counter = 0;
+    contacts = stored ? resetIDs(stored) : [];
+  }
+
+  var resetIDs = function(array) {
+    array = array.map(function(contact) {
+      contact.id = counter++;
+      return contact;
+    });
+    return array;
+  };
+
+  var save = function() {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  };
 
   var add = function(contact) {
     contact.id = counter++;
     contacts.push(contact);
+    save(); // save to local storage
   };
 
   var getAll = function() {
     return contacts;
   };
+
+  init();
 
   return {
     add: add,
